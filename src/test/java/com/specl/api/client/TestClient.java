aba52ea639a457118e7a8c9d5953339d3ecd5baf
Jock <jockhuang@gmail.com>
2012-02-08 16:17:59 +0800

    add pool test

--- src/test/java/com/specl/api/client/TestClient.java
+++ src/test/java/com/specl/api/client/TestClient.java
@@ -1,8 +1,14 @@
 package com.specl.api.client;
 
 import java.net.InetSocketAddress;
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
-import org.apache.log4j.Logger;
+import org.apache.commons.pool.PoolableObjectFactory;
+import org.apache.commons.pool.impl.GenericObjectPool;
 import org.apache.mina.core.future.ConnectFuture;
 import org.apache.mina.core.future.ReadFuture;
 import org.apache.mina.core.service.IoConnector;
@@ -10,53 +16,127 @@ import org.apache.mina.core.session.IoSession;
 import org.apache.mina.core.session.IoSessionConfig;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
 import org.apache.mina.filter.codec.serialization.ObjectSerializationCodecFactory;
-import org.apache.mina.filter.logging.LoggingFilter;
 import org.apache.mina.transport.socket.nio.NioSocketConnector;
 
-public final class TestClient {
-    private static final Logger logger = Logger.getLogger(TestClient.class);
+import com.specl.model.Board;
+import com.specl.utils.JsonUtil;
+
+/**
+ * @author litianyi
+ * @version 2012-2-8
+ */
+public class TestClient {
+
+    public TestClient(String message, GenericObjectPool pool) {
+
+        try {
+            // session = future.getSession();
+            IoSession session = (IoSession) pool.borrowObject();
+            session.write(message);
+            ReadFuture readFuture = session.read();
+            // // 阻塞线程 同步获得数据
+            if (readFuture.awaitUninterruptibly(3000)) {
+                System.out.println(readFuture.getMessage());
+            }
+
+            pool.returnObject(session);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    
+    public TestClient(String message, IoConnector connector) {
+
+        try {
+            IoSessionConfig cfg = connector.getSessionConfig();
+            // 这个方法设置IoSession 的read()方法是否可用，默认是false。
+            cfg.setUseReadOperation(true);
+            ConnectFuture future = connector.connect(new InetSocketAddress("localhost", 8888));
+            // 阻塞线程等待链接，使异步转变成同步
+            future.awaitUninterruptibly();
+            
+            // session = future.getSession();
+            IoSession session =future.getSession();
+            session.write(message);
+            ReadFuture readFuture = session.read();
+            // // 阻塞线程 同步获得数据
+            if (readFuture.awaitUninterruptibly(3000)) {
+                System.out.println(readFuture.getMessage());
+            }
+
+            session.close(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
     /**
      * 
      * @param args
+     * @throws Exception
      */
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        IoConnector connector;
-        IoSession session;
-
+    public static void main(String[] args) throws Exception {
         ConnectFuture future;
-
-        connector = new NioSocketConnector();
-        connector.getFilterChain().addLast("logger", new LoggingFilter());
+        IoConnector connector = new NioSocketConnector();
         connector.getFilterChain().addLast("codec", new ProtocolCodecFilter(new ObjectSerializationCodecFactory()));
-        // ApiClientHandler handler = new ApiClientHandler(APIClient.this);
-        // connector.setHandler(handler);
-        // connector.setHandler(new ServiceProtocolHandler());
         IoSessionConfig cfg = connector.getSessionConfig();
+        // 这个方法设置IoSession 的read()方法是否可用，默认是false。
         cfg.setUseReadOperation(true);
-        future = connector.connect(new InetSocketAddress("localhost", 1234));
-
+        future = connector.connect(new InetSocketAddress("localhost", 8888));
+        // 阻塞线程等待链接，使异步转变成同步
         future.awaitUninterruptibly();
-        session = future.getSession();
+        BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>();
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(8, 8, 1, TimeUnit.DAYS, queue);
         
-        // message.setMethod("/board/getBoard");
-        // Board b = new Board();
-        // b.setBoardId(12);
-        // message.setParam(JsonUtil.toJson(b));
-        Integer[] ids = new Integer[10000000];
-        session.write(ids);
-       
-        ReadFuture readFuture = session.read();
-        Object m = null;
-        if (readFuture.awaitUninterruptibly(3000)) {
-            m = readFuture.getMessage();
-            logger.info("m1:"+m);
-            Object m2 = readFuture.getMessage();
-            logger.info("m2:"+m2);
-        }
-        session.close(true);
-        session.getService().dispose(false);
-       
-    }
+        PoolableObjectFactory factory = new PoolFactory(connector);
+        GenericObjectPool pool = new GenericObjectPool(factory, 10, GenericObjectPool.WHEN_EXHAUSTED_GROW, 10, true,
+                true);
 
+       
+        for (int index = 0; index < 100; index++) {
+
+            TestRunnable run = new TestRunnable(pool) ;
+            
+            executor.execute(run);
+        }
+        
+        // pool.close();
+         
+         
+        /*
+        for (int index = 0; index < 100; index++) {
+
+            TestRunnable2 run = new TestRunnable2(connector) ;
+            
+            executor.execute(run);
+        }
+        */
+        executor.shutdown();
+        executor.awaitTermination(10, TimeUnit.MINUTES);
+        System.out.println("over!");
+    }
+    
+}
+class TestRunnable implements Runnable{
+    GenericObjectPool pool;
+    public TestRunnable(GenericObjectPool pool){
+        this.pool = pool;
+    }
+    public void run() {
+        Random r = new Random();
+        new TestClient("HELLO" + r.nextInt(), pool);
+    }
+}
+
+class TestRunnable2 implements Runnable{
+    IoConnector future;
+    public TestRunnable2(IoConnector pool){
+        this.future = pool;
+    }
+    public void run() {
+        Random r = new Random();
+        new TestClient("HELLO" + r.nextInt(), future);
+    }
 }
